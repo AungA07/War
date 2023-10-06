e1ff2a78bddfa981129a6e614fe5878e5bca03e8
AungA07 <142325709+AungA07@users.noreply.github.com>
2023-10-06 10:01:47 -0400

    Score rounds and save cards

---
+++ war_with_ppl_copy.py
@@ -0,0 +1,63 @@
+# what we do
+# store won cards from each round of war
+# Compare stored cards for winner of the round give winner a point (for winning the round)
+# Compare points (from rounds) to get complete winner
+player = "John"
+won_cards1 = []
+won_cards2 = []
+p1_points = []
+p2_points = []
+card1 = [1]
+card2 = [2]
+if card1 > card2:
+    won_cards1.append(card1)
+    won_cards1.append(card2)
+if card2 > card1:
+    won_cards2.append(card1)
+    won_cards2.append(card2)
+else:
+    # tie later
+    pass
+
+
+# round 1
+# round points
+def round_points(cards1, cards2, player1):
+    if len(cards1) > len(cards2):
+        p1_points.append(1)
+        print(f" {player1} win's the round")
+        print(f"{player1} has {len(p1_points)} points\n")
+    elif len(cards2) > len(cards1):
+        p2_points.append(1)
+        print("The computer win's the round")
+        print(f"computer has {len(p2_points)} points\n")
+
+
+round_points(won_cards1, won_cards2, player)
+# actually giving points here
+
+# round 2
+round_points(won_cards1, won_cards2, player)
+# giving points
+
+# printing winners
+if len(p1_points) != 2:
+    pass
+else:
+    print(f"{player} wins war!")
+    quit()
+if len(p2_points) != 2:
+    pass
+else:
+    print("Computer wins war!")
+    quit()
+
+# round 3 The tie
+round_points(won_cards1, won_cards2, player)
+# give points
+
+# print the overall winner
+if len(p1_points) == 2:
+    print(f"{player} wins war!")
+elif len(p2_points) == 2:
+    print("Computer wins war!")
